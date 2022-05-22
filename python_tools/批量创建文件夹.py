3b55202ef48be112d16635e98a26fcc389bcd9bd
wuxiaonan <2270034975@qq.com>
2022-05-22 19:04:03 +0800

    20220522 from company

---
+++ python_tools/批量创建文件夹.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+import random
+
+
+os.chdir(r'F:\测厚\图片')
+for dir_name in range(17, 41):
+    for i in range(4):
+        pic_name = list(os.listdir(f'./图片'))[random.randint(1, len(os.listdir(f'./图片')))]
+        shutil.move(f'./图片/{pic_name}', f'./{str(dir_name)}/{pic_name}')
