99e2601abb930b8d6133a6a9d9969560fe2c9401
wuxiaonan <2270034975@qq.com>
2022-05-23 10:58:29 +0800

    20220523 from company

--- python_tools/打印doc.py
+++ python_tools/打印doc.py
@@ -2,11 +2,11 @@ from printall import printer_loading
 import os,  time
 
 
-os.chdir(r'C:\Users\Administrator\Desktop\2021年自动模板')
-for file in os.listdir():
-    if file.endswith('.doc'):
-        if "模板" not in file and "合格证" not in file:
-            for i in range(2):
+os.chdir(r'F:\测厚')
+for i in range(2):
+    for file in os.listdir():
+        if file.endswith('.docx'):
+            if "模板" not in file:
                 printer_loading(file)
                 time.sleep(5)
 
