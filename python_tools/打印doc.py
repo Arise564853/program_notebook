f9686bcd5955988f8848a8e79b73cd9b6f5336db
wuxiaonan <2270034975@qq.com>
2022-05-25 17:38:20 +0800

    20220525 from company

--- python_tools/打印doc.py
+++ python_tools/打印doc.py
@@ -2,11 +2,11 @@ from printall import printer_loading
 import os,  time
 
 
-os.chdir(r'F:\测厚')
-for i in range(2):
-    for file in os.listdir():
-        if file.endswith('.docx'):
-            if "模板" not in file:
+os.chdir(r'D:\Documents\Tencent Files\277038938\FileRecv')
+for file in os.listdir():
+    if file.endswith('.docx'):
+        if "模板" not in file:
+            for i in range(2):
                 printer_loading(file)
                 time.sleep(5)
 
